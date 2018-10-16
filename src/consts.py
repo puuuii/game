2dfd37b80add6eaa34e7bc853a145ce09a7b55c5
shota <rx4_raffin@yaoo.coljp>
2018-10-16 09:01:48 +0900

    feat: map系処理にpkl化追加

--- src/consts.py
+++ src/consts.py
@@ -6,12 +6,19 @@ SCR_RECT = Rect(0, 0, 1024,768)
 FPS = 30
 
 # マップ系
-LENGTH_OF_ONE_SIDE = 2 ** 5#16
+LENGTH_OF_ONE_SIDE = 2 ** 6#16
 PIXCEL_OF_ONE_SIDE = 16
-ROOP_MAKING = 1
+ROOP_MAP_MAKING = 10
 SEA = 0
 SAND = 1
 GLASS = 2
 FOREST = 3
 MOUNTAIN = 4
 RIVER = 5
+PATH_STAGE = '../data/stage.pkl'
+PATH_SEA = '../img/sea1.png'
+PATH_SAND = '../img/sand1.png'
+PATH_GLASS = '../img/glass1.png'
+PATH_FOREST = '../img/forest1.png'
+PATH_MOUNTAIN = '../img/mountain1.png'
+PATH_RIVER = '../img/river1.png'
--- src/map/map.py
+++ src/map/map.py
@@ -1,23 +1,29 @@
-from pygame.image import load
 import pickle
 import numpy as np
+import os
 from src.consts import *
+from pygame.image import load
 
 
 class Map:
     """マップクラス"""
 
     def __init__(self):
-        self.stage = self._create_stage(ROOP_MAKING)
-        self.trrain_converter = {SEA: load('../img/sea1.png').convert(),
-                                 SAND: load('../img/sand1.png').convert(),
-                                 GLASS: load('../img/glass1.png').convert(),
-                                 FOREST: load('../img/forest1.png').convert(),
-                                 MOUNTAIN: load('../img/mountain1.png').convert(),
-                                 RIVER: load('../img/river1.png').convert()}
+        self.stage = self._create_stage(ROOP_MAP_MAKING)
+        self.trrain_converter = {SEA: load(PATH_SEA).convert(),
+                                 SAND: load(PATH_SAND).convert(),
+                                 GLASS: load(PATH_GLASS).convert(),
+                                 FOREST: load(PATH_FOREST).convert(),
+                                 MOUNTAIN: load(PATH_MOUNTAIN).convert(),
+                                 RIVER: load(PATH_RIVER).convert()}
 
     def _create_stage(self, roop_making):
         """ステージ作成"""
+
+        # すでにステージが存在するならそれを返す
+        if os.path.exists(PATH_STAGE):
+            with open(PATH_STAGE, mode='rb') as f:
+                return pickle.load(f)
 
         # 海と陸の作成
         stage = np.random.randint(2, size=(LENGTH_OF_ONE_SIDE, LENGTH_OF_ONE_SIDE))
@@ -26,24 +32,31 @@ class Map:
                 for c in range(LENGTH_OF_ONE_SIDE):
                     stage[r][c] = self._make_terrain(stage, r, c, SAND)
 
+        # ステージのpkl化
+        with open(PATH_STAGE, mode='wb') as f:
+            pickle.dump(stage, f)
+
         return stage
 
     def _make_terrain(self, stage, r, c, value):
         """周囲の地形から特定地形作成"""
-        # 外周は海とする
-        try:
-            counter = 0
-            for i in range(-1, 1, 1):
-                for j in range(-1, 1, 1):
-                    if i == 0 and j == 0:
-                        continue
-                    if stage[r+i][c+j] == value:
-                        counter += 1
-        except:
-            return SEA
+        # 周囲8セルに特定地形が含まれる数をカウント
+        counter = 0
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                # 自身セルは除外
+                if i == 0 and j == 0:
+                    continue
+                # 周囲がステージ外(ステージの外枠)の場合は海
+                if (r+i < 0) or (r+1 >= stage.shape[0])\
+                        or (c+j < 0) or (c+j >= stage.shape[1]):
+                    return SEA
+                # カウント
+                if stage[r+i][c+j] == value:
+                    counter += 1
 
         # 要素数8のリスト要素のうちカウンターの個数だけvalueにし、そこからランダムで値を取り出す
-        random_array = np.r_[np.full(counter, value), np.full(8-counter, 0)]
+        random_array = np.r_[np.full(counter*3, value), np.full((8-counter)*2, 0)]
 
         return np.random.choice(random_array, 1)
 
