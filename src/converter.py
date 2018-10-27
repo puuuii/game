efba7122dc382d560c4fee5c8f4d46fb339f599e
puuuii <rx4_raffin@yahoo.co.jp>
2018-10-28 08:38:58 +0900

    feat: 変換クラスの継承化

--- src/converter.py
+++ src/converter.py
@@ -2,62 +2,53 @@ from src.consts import *
 from pygame.image import load
 
 
-class MapImage:
+class ConverterBase:
+    """画像変換基底クラス"""
+    _instance = None
+
+    def __new__(cls, converter=None):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+
+        return cls._instance
+
+    def __init__(self, converter):
+        self.converter = converter
+
+    def get_converter(self):
+        return self.converter
+
+
+class MapImage(ConverterBase):
     """マップ画像変換クラス"""
-    _instance = None
 
     def __init__(self):
-        self.converter = {SEA: load(PATH_SEA).convert(),
-                         SAND: load(PATH_SAND).convert(),
-                         GLASS: load(PATH_GLASS).convert(),
-                         FOREST: load(PATH_FOREST).convert(),
-                         MOUNTAIN: load(PATH_MOUNTAIN).convert(),
-                         RIVER: load(PATH_RIVER).convert()}
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-
-        return cls._instance
-
-    def get_converter(self):
-        return self.converter
+        converter = {SEA: load(PATH_SEA).convert(),
+                     SAND: load(PATH_SAND).convert(),
+                     GLASS: load(PATH_GLASS).convert(),
+                     FOREST: load(PATH_FOREST).convert(),
+                     MOUNTAIN: load(PATH_MOUNTAIN).convert(),
+                     RIVER: load(PATH_RIVER).convert()}
+        super().__init__(converter)
 
 
-class PlayerImage:
+class PlayerImage(ConverterBase):
     """プレーヤー画像変換クラス"""
-    _instance = None
 
     def __init__(self):
-        self.converter = {DIRECTION_UP: load(PATH_IMAGE_PLAYER_UP).convert_alpha(),
-                          DIRECTION_RIGHT: load(PATH_IMAGE_PLAYER_RIGHT).convert_alpha(),
-                          DIRECTION_DOWN: load(PATH_IMAGE_PLAYER_DOWN).convert_alpha(),
-                          DIRECTION_LEFT: load(PATH_IMAGE_PLAYER_LEFT).convert_alpha()}
+        converter = {DIRECTION_UP: load(PATH_IMAGE_PLAYER_UP).convert_alpha(),
+                     DIRECTION_RIGHT: load(PATH_IMAGE_PLAYER_RIGHT).convert_alpha(),
+                     DIRECTION_DOWN: load(PATH_IMAGE_PLAYER_DOWN).convert_alpha(),
+                     DIRECTION_LEFT: load(PATH_IMAGE_PLAYER_LEFT).convert_alpha()}
+        super().__init__(converter)
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
 
-        return cls._instance
-
-    def get_converter(self):
-        return self.converter
-
-class NationImage:
+class NationImage(ConverterBase):
     """国家画像変換クラス"""
-    _instance = None
 
     def __init__(self):
-        self.converter = {NATION_LEVEL_VILLAGE: load(PATH_IMAGE_VILLAGE).convert_alpha(),
-                          NATION_LEVEL_TOWN: load(PATH_IMAGE_TOWN).convert_alpha(),
-                          NATION_LEVEL_CASTLE_TOWN: load(PATH_IMAGE_CASTLE_TOWN).convert_alpha(),
-                          NATION_LEVEL_CASTLE: load(PATH_IMAGE_CASTLE).convert_alpha()}
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-
-        return cls._instance
-
-    def get_converter(self):
-        return self.converter
+        converter = {NATION_LEVEL_VILLAGE: load(PATH_IMAGE_VILLAGE).convert_alpha(),
+                     NATION_LEVEL_TOWN: load(PATH_IMAGE_TOWN).convert_alpha(),
+                     NATION_LEVEL_CASTLE_TOWN: load(PATH_IMAGE_CASTLE_TOWN).convert_alpha(),
+                     NATION_LEVEL_CASTLE: load(PATH_IMAGE_CASTLE).convert_alpha()}
+        super().__init__(converter)
